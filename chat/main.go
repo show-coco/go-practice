f57232ddfaa9482dc0d2f9786da29c20027eab39
show <show75@ezweb.ne.jp>
2020-11-29 16:45:40 +0900

    チャットルームの生成と利用

--- chat/main.go
+++ chat/main.go
@@ -25,9 +25,14 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
 
-	// Webサーバを開始します
+	// チャットルームを開始
+	go r.run()
+
+	// Webサーバを起動
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
